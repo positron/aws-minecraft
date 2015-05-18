9a083ef024b5cfeba0ad22cd989ff8d7c62c0638
Philip Jagielski <philipjagielski@gmail.com>
2015-05-18 17:09:51 -0500

    Use stream functionality to split and inspect stdout and stderr

--- server.js
+++ server.js
@@ -2,8 +2,16 @@
 
 var spawn = require('child_process').spawn;
 
-var mcServer = spawn('python', [], { stdio: ['inherit', 'inherit', 'inherit'] });
-
+var mcServer = spawn('python', [], { stdio: ['inherit', 'pipe', 'pipe'] });
+mcServer.stdout.on('data', onMCData);
+mcServer.stderr.on('data', onMCData);
 mcServer.on('exit', function onExit() {
   console.log('server exited');
 });
+
+mcServer.stdout.pipe(process.stdout);
+mcServer.stderr.pipe(process.stderr);
+
+function onMCData(data) {
+  // process stuff here
+}
