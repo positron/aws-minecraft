efd4b8faa4663e91ac0af9a898c447703eb53c72
Philip Jagielski <philipjagielski@gmail.com>
2015-11-03 04:51:28 +0000

    Factor out login error handling

--- static/aws-minecraft.js
+++ static/aws-minecraft.js
@@ -3,10 +3,17 @@
 
 PW_URL="https://2u20yskc8i.execute-api.us-east-1.amazonaws.com/prod/fetch_minecraft_secret";
 
+function authError(errorMsg) {
+   // TODO: display nicely somewhere
+   alert(errorMsg);
+   // TODO: reset state of UI to beginning where auth is required
+}
+
 $(function() {
    $('#start-button').click( function() {
-      $(this).html("Booting...");
       pw = window.prompt("Password");
+      $(this).html("Logging on...");
+
       post_data = { password: pw };
       console.log(post_data);
       iam_access_key = ""; // TODO: don't think this is a very javascript way to do this...
@@ -16,20 +23,26 @@ $(function() {
          type: "POST",
          data: JSON.stringify(post_data),
          success: function(data) {
-            if (data.User) { // data returned by Amazon for some reason?
-               console.log('ERROR: ' + data.User);  // TODO handle error
+            // AWS returns errors this way sometimes
+            if (data.User) {
+               console.log('ERROR: ' + data.User);
+                authError('Error logging in: ' + data.User)
             }
-            else if (data.errorMessage) { // thrown by lambda function (.fail returns 200 for some reason?)
-               console.log('ERROR: ' + data.errorMessage);  // TODO handle error
-            } else {
+            // lambda's .fail returns 200 for some reason (API Gateway is probably misconfigured)
+            else if (data.errorMessage) {
+               console.log('ERROR: ' + data.errorMessage);
+               authError(data.errorMessage);
+            } else if (data.access_key_id && data.secret_access_key){
                console.log(data.access_key_id);
                console.log(data.secret_access_key);
+            } else {
+               console.log('Returned 200 but response is malformed: ' + data);
+               authError('Internal programming error');
             }
          },
          error: function(jqXHR, status, error) {
-            // TODO handle error. General error like server is down
-            console.log(status);
-            console.log(error);
+            console.log('ERROR: ' + status + ': ' + error);
+            authError('Error logging in. Try again?')
          }
       });
    });
