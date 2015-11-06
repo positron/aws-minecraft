ae96d59c7d8edd4a6d323b0c0b92cff03459f3e4
Philip Jagielski <philipjagielski@gmail.com>
2015-11-06 15:47:42 -0600

    Save credentials to localStorage
    Also refactor a little bit.

--- static/aws-minecraft.js
+++ static/aws-minecraft.js
@@ -1,40 +1,79 @@
+// TODO: hacking together this code for now.
+//       when it is all working I will make a proper state machine UI out of it using promises or some library or something
+
+
 // TODO: stub out UI functionality?
 //window.setTimeout(func, delay);
 
-PW_URL="https://2u20yskc8i.execute-api.us-east-1.amazonaws.com/prod/fetch_minecraft_secret";
-
-function authError(errorMsg) {
-   // TODO: display nicely somewhere
-   alert(errorMsg);
-   // TODO: reset state of UI to beginning where auth is required
-}
+LAMBDA_URL="https://2u20yskc8i.execute-api.us-east-1.amazonaws.com/prod/fetch_minecraft_secret";
 
 $(function() {
-   $('#start-button').click( function() {
-      pw = window.prompt("Password");
-      $(this).html("Logging on...");
+   START_BUTTON = $('#start-button');
 
-      post_data = { password: pw };
-      console.log(post_data);
-      iam_access_key = ""; // TODO: don't think this is a very javascript way to do this...
-      iam_secret_key = "";
+   START_BUTTON.click( function() {
+      // callback hell right now, will make more imperitive in the future
+      var creds = getCreds();
+      console.log('Got creds: ' + creds.access_key_id);
+      startServer(creds);
+   });
+
+   function authError(errorMsg) {
+      // TODO: display nicely somewhere
+      alert(errorMsg);
+      // TODO: reset state of UI to beginning where auth is required
+   }
+
+   // Save creds in HTML5 localstorage so we don't have to prompt for the password every time
+   function saveCreds(data) {
+      if (window.localStorage) {
+         creds = {
+            'access_key_id': data.access_key_id,
+            'secret_access_key': data.secret_access_key
+         };
+         localStorage.setItem('creds', JSON.stringify(creds));
+      }
+      else {
+         console.log('localStorage isn\'t supported. Can\'t save credentials.');
+      }
+   }
+
+   // Get the IAM credentials to turn the server on. Either from localstorage
+   // or by providing the right password to a lambda function.
+   function getCreds() {
+      START_BUTTON.html("Authenticating...");
+      if (window.localStorage && localStorage.creds) {
+         creds = JSON.parse(localStorage.creds);
+         return creds
+      } else {
+         return getCredsFromLabmda();
+      }
+   }
+
+   function getCredsFromLabmda() {
+      var pw = window.prompt("Password");
+
+      var post_data = { password: pw };
       $.ajax({
-         url: PW_URL,
+         url: LAMBDA_URL,
          type: "POST",
          data: JSON.stringify(post_data),
          success: function(data) {
             // AWS returns errors this way sometimes
             if (data.User) {
                console.log('ERROR: ' + data.User);
-                authError('Error logging in: ' + data.User)
+               authError('Error logging in: ' + data.User)
             }
             // lambda's .fail returns 200 for some reason (API Gateway is probably misconfigured)
             else if (data.errorMessage) {
                console.log('ERROR: ' + data.errorMessage);
                authError(data.errorMessage);
             } else if (data.access_key_id && data.secret_access_key){
-               console.log(data.access_key_id);
-               console.log(data.secret_access_key);
+               var creds = {
+                  'access_key_id': data.access_key_id,
+                  'secret_access_key': data.secret_access_key
+               };
+               saveCreds(creds);
+               startServer(creds);
             } else {
                console.log('Returned 200 but response is malformed: ' + data);
                authError('Internal programming error');
@@ -45,5 +84,9 @@ $(function() {
             authError('Error logging in. Try again?')
          }
       });
-   });
+   }
+
+   function startServer(creds) {
+      START_BUTTON.html("Starting server...");
+   }
 });
