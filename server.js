cd82ebcdb3031f2bf54502c2c6e88380f479051b
Philip Jagielski <philipjagielski@gmail.com>
2015-11-07 04:56:51 +0000

    node server wraps minecraft with no extra functionality
    Node v5.0.0.

--- server.js
+++ server.js
@@ -3,19 +3,26 @@
 var fs = require('fs');
 var spawn = require('child_process').spawn;
 
-var mcServer = spawn('python', [], { stdio: ['inherit', 'pipe', 'pipe'] });
+var mcServer = spawn('java',
+      ['-Xmx1024M', '-Xms1024M', '-jar', 'minecraft_server.1.8.8.jar', 'nogui'],
+      { cwd: '/home/vagrant/bin/', stdio: ['pipe', 'pipe', 'pipe']});
+
 mcServer.stdout.on('data', onMCData);
 mcServer.stderr.on('data', onMCData);
+process.stdin.on('data', onMCData);
 mcServer.on('exit', function onExit() {
-  console.log('server exited');
+  console.log('Minecraft server exited');
+  process.exit(0);
 });
 
 mcServer.stdout.pipe(process.stdout);
 mcServer.stderr.pipe(process.stderr);
+process.stdin.pipe(mcServer.stdin);
 
 var logfile = fs.createWriteStream('log.txt');
 mcServer.stdout.pipe(logfile);
 mcServer.stderr.pipe(logfile);
+process.stdin.pipe(logfile);
 
 function onMCData(data) {
   // process stuff here
